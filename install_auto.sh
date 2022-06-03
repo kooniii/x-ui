e90fadbf5c545cd9a9b1aea9f680c23f3f1e9fb0
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-06-03 23:54:39 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -85,8 +85,10 @@ install_base() {
 config_after_install() {
     echo -e "${yellow}出于安全考虑，安装/更新完成后需要强制修改端口与账户密码${plain}"
     read -p "确认是否继续?[y/n]": config_confirm
+    echo "y"
     if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
         read -p "请设置您的账户名:" config_account
+        echo reno1314
         echo -e "${yellow}您的账户名将设定为:${config_account}${plain}"
         read -p "请设置您的账户密码:" config_password
         echo -e "${yellow}您的账户密码将设定为:${config_password}${plain}"
