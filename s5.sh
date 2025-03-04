3a35321162608d31a92111b401fbd567d2d36139
kooniii <132741450+kooniii@users.noreply.github.com>
2025-03-04 23:44:54 +0800

    Update s5.sh

--- s5.sh
+++ s5.sh
@@ -130,7 +130,7 @@ sic_optimization() {
 
 # 固定端口设置
 port_set() {
-    port=10999
+    port=26263
 }
 
 port_exist_check() {
@@ -150,8 +150,8 @@ port_exist_check() {
 
 # 固定用户名和密码设置
 user_set() {
-    user="bb33"
-    passwd="bb33"
+    user="aa33"
+    passwd="aa33"
 }
 
 install_ss5() {
